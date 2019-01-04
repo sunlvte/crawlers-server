5de8a4f7799ae2f57c81097beb02f503dd4ff504
sunlvte <15858260500@163.com>
2019-01-04 13:45:26 +0800

    nochild

--- app.js
+++ app.js
@@ -10,17 +10,16 @@ const spiders_order = require('./lib/spiders_order');
 const spiders_ticker = require('./lib/spiders_ticker');
 const config = require('./config/start');
 const connectMongo = require('./models/connect')
-const childprocess = require('child_process')
+
 const log = require('./lib/log')
 const util = require('./lib/util')
 global.config = config;
 connectMongo()
-let worker = childprocess.fork('./lib/childprocess.js')
-worker.on('exit', (i) => {
-	log(`[childprocess error] pid=${i}  时间：${util.DateNow()}`, 'error')
-	worker = childprocess.fork('./lib/childprocess.js')
-	log(`[create new childprocess info] pid=${i}  时间：${util.DateNow()}`, 'info')
-})
+const spiders_order = require('./lib/spiders_order');
+const spiders_ticker = require('./lib/spiders_ticker');
+
+spiders_order()
+spiders_ticker()
 
 
 
